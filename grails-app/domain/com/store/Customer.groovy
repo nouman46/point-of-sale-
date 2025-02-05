3c393fb035b89434c746a028ae2a0e50a84889e6
786 <bajwanouman7@gmail.com>
2025-02-05 13:27:53 +0500

    nouman

--- grails-app/domain/com/store/Customer.groovy
+++ grails-app/domain/com/store/Customer.groovy
@@ -1,17 +1,18 @@
 package com.store
 
 class Customer {
-
+    String customerName
     String productName
     String productDescription
     String productBarcode
     BigDecimal productPrice
+    BigDecimal totalPrice
     Integer productQuantity
 
         static constraints = {
+            customerName blank: false, maxSize: 255
             productName blank: false, maxSize: 255
             productDescription nullable: true, maxSize: 500
-            productSKU blank: false, unique: true, maxSize: 50
             productBarcode blank: false, unique: true, maxSize: 50
             productPrice min: 0.01, scale: 2
         }
